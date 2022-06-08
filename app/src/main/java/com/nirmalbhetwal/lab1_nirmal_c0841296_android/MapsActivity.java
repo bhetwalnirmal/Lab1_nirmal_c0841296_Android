d06f70a974f1811358f2b3c74d40c014109c07bf
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-06-07 22:20:04 -0400

    removed unused comment

--- app/src/main/java/com/nirmalbhetwal/lab1_nirmal_c0841296_android/MapsActivity.java
+++ app/src/main/java/com/nirmalbhetwal/lab1_nirmal_c0841296_android/MapsActivity.java
@@ -129,15 +129,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
 
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
         LatLng toronto = new LatLng(43.65, -79.38);
         LatLng brampton = new LatLng(43.7315, -79.7624);
         LatLng mississauga = new LatLng(43.5890, -79.6441);
